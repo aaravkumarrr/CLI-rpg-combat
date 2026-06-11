195b1c219b96fd1a0a6cc07ea1609a542201355c
AK <aarav1102006@gmail.com>
2026-06-11 12:58:45 +0800

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,25 +1,40 @@
-from character import Character
+import character
 import random
 
-class Enemy(Character):
-    def __init__(self, name, health, max_health, attack_power):
-        super().__init__(name, health, max_health, attack_power)
+class Enemy(character.Character):
+    def take_turn(self, target):
+        action = random.choice([self.attack,self.special_ability])
+        action(target)
     
-    def taketurn(self, target):
-        pass
-    
+
 class Goblin(Enemy):
-    def __init__(self, name):
-        self.name = name
+    def __init__(self):
+        self.name = "Goblin"
+        self.max_health = 50
         self.health = 50
         self.attack_power = 8
-
+        super().__init__(self.name, self.health, self.max_health, self.attack_power)
+    
     def attack(self, target):
-        print("Goblin has struck!")
+        print(f"{self.name} attacked {target.name}")
         target.take_damage(self.attack_power)
-        print("Goblin has struck again!")
-        target.take_damage(self.attack_power)       
+    
+    def special_ability(self, target):
+        print(f"{self.name} dodges the attack.")
 
-    def special_ability():
-        print("Goblin dodges the attack.")
-        pass 
+class Dragon(Enemy):
+    def __init__(self):
+        self.name = "Dragon"
+        self.max_health = 200
+        self.health = 200
+        self.attack_power = 20
+        super().__init__(self.name, self.health, self.max_health, self.attack_power)
+    
+    def attack(self, target):
+        print(f"{self.name} attacked {target.name}")
+        target.take_damage(self.attack_power)
+
+    def special_ability(self, target):
+        print(f"{self.name} breathes fire.")
+        target.take_damage(self.attack_power * 2)
+
